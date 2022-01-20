31fadf29da7263377d5685edc17636e1c193b21f
Túlio Moura De Battisti <tulio150@hotmail.com>
2022-01-20 15:51:27 -0300

    Versioning

--- Tibia Time Machine/version.h
+++ Tibia Time Machine/version.h
@@ -1,12 +1,12 @@
 #pragma once
 #include <windows.h>
 
-#define RES_VERSION 6,4,0,0
-#define STR_VERSION "6.4"
-#define TICKET "TTM64"
+#define RES_VERSION 6,5,0,0
+#define STR_VERSION "6.5"
+#define TICKET "TTM65"
 
 #define SITE_URL "otland.net"
-#define DEV_YEAR "2021"
+#define DEV_YEAR "2022"
 
 #define LATEST 1120
 #define VERSION_EXAMPLES "7.0, 7.55, 8.3, 9.86, 10.54, 11.00, 11.20"
